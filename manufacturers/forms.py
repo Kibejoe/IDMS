2e779f0deb45ddcdba4429558c5eef48cedf7a05
Joseph Kibe <kibejoe254@gmail.com>
2025-07-11 14:51:03 +0300

    Created utility form

--- manufacturers/forms.py
+++ manufacturers/forms.py
@@ -1,7 +1,32 @@
 from django import forms
-from .models import EstablishmentProfile
+from .models import UtilityConsumption
 
-class EstablishmentForm(forms.ModelForm):
+class UtilityConsumptionForm(forms.ModelForm):
     class Meta:
-        model = EstablishmentProfile
+        model = UtilityConsumption
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+        for field_name, field in self.fields.items():
+            if not isinstance(field.widget, (forms.CheckboxInput, forms.RadioSelect, forms.FileInput)):
+                field.widget.attrs['class'] = 'form-control'
+
+    def clean(self):
+        cleaned_data = super().clean()
+        utility_type = cleaned_data.get('utility_type') 
+        other_name = cleaned_data.get('other_utility_name')
+
+        for field_name in ['quantity_used', 'value_ksh', 'max_capacity_required']:
+            value = cleaned_data.get(field_name)
+
+            if value is not None and value <= 0:
+                self.add_error(field_name, f"{field_name} must be greater than 0")
+
+        if utility_type == 'other' and not other_name:
+            self.add_error('other_utility_name', 'please specify the utility name.')
+
+        return cleaned_data
+
+    
--- manufacturers/views.py
+++ manufacturers/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .models import EstablishmentProfile
-from .forms import EstablishmentForm
+from .models import UtilityConsumption
+from .forms import UtilityConsumptionForm
+
+def utility(request):
+    if request.method == 'POST':
+        form = UtilityConsumptionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('utility')
+        
+    else:
+        form = UtilityConsumptionForm()
+    return render(request, 'application/utility.html', {'form': form})
